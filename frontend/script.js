cfc117519b7a24676b9336ff17e0d141b74fad53
Ryavnn <ryanisnjoroge@gmail.com>
2025-01-19 18:58:18 +0300

    Sign up form changed

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,44 @@
-const passwordField = document.getElementById('exampleInputPassword1');
-    const togglePasswordIcon = document.getElementById('togglePassword');
 
-    togglePasswordIcon.addEventListener('click', function() {
-        // Toggle the type attribute
-        const type = passwordField.type === 'password' ? 'text' : 'password';
-        passwordField.type = type;
+const togglePasswordIcon = document.getElementById("togglePassword");
+const form = document.getElementsByClassName("signup-form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("exampleInputEmail1");
+const password = document.getElementById("exampleInputPassword");
+const confirmPassword = document.getElementById("exampleInputPassword1");
+const errors = document.getElementsByClassName("error")[0]
 
-        // Toggle the icon
-        this.textContent = type === 'password' ? '👁️' : '🙈'; // Optional: Change the icon based on visibility
-    });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const confirmPasswordValue = confirmPassword.value.trim();
+
+  let error = [];
+  if (!firstNameValue) {
+    error.push("First name is required");
+  }
+  if (!lastNameValue) {
+    error.push("Last name is required");
+  }
+  if (!emailValue) {
+    error.push("Email is required");
+  } else if (!/\S+@\S+\.\S+/.test(emailValue)) {
+    error.push("Invalid email");
+  }
+  if (passwordValue.length < 6) {
+    error.push("Password must be at least 6 characters");
+  }
+  if (passwordValue !== confirmPasswordValue) {
+    error.push("Passwords do not match");
+    }
+    if (error.length > 0) { 
+        errors.innerHTML = error.join('<br>')
+
+    } else {
+        alert("Form submitted successfully")
+    }
+});
+
